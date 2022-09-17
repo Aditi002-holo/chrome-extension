b5c375f93cb65dbb979439519923041e48af4c65
Aditi002-holo <aditideshpande2908@gmail.com>
2022-09-17 15:22:38 +0530

    Add renderLeads()

--- app.js
+++ app.js
@@ -1,25 +1,32 @@
 const inputBtn = document.getElementById("input-btn");
 const inputEle = document.getElementById("input-el");
 const ulEle = document.getElementById("ul-el");
-let myLeads = ["www.awesomelead.com","www.epiclead.com", "www.greatlead.com"];
-let listItems = "";
+let myLeads = [];
+
 
 inputBtn.addEventListener("click",function() {
     myLeads.push(inputEle.value);
-    // console.log(myLeads);
+    renderLeads();
+
 })
 
-for(let i = 0; i < myLeads.length; i++) {
-    listItems += "<li>" + myLeads[i] + "</li>";
-    // alternative
-    // const li = document.createElement("li");
-    // li.textContent = myLeads[i];
-    // ulEle.append(li);
+function renderLeads() {
+    let listItems = "";
+    for(let i = 0; i < myLeads.length; i++) {
+        listItems += "<li>" + myLeads[i] + "</li>"; 
+    }
+    ulEle.innerHTML = listItems;
 }
 
-ulEle.innerHTML = listItems;
+
 
 
 // www.awesomelead.com
 // www.epiclead.com
 // www.greatlead.com
+
+
+// alternative
+// const li = document.createElement("li");
+// li.textContent = myLeads[i];
+// ulEle.append(li);
