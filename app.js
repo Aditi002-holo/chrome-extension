1a90844b02736b1ad57bf56138fe04fe8c94ab8e
Aditi002-holo <aditideshpande2908@gmail.com>
2022-09-17 15:17:25 +0530

    Render saved inputs as list items

--- app.js
+++ app.js
@@ -1,5 +1,25 @@
-const saveBtn = document.getElementById("input-btn");
+const inputBtn = document.getElementById("input-btn");
+const inputEle = document.getElementById("input-el");
+const ulEle = document.getElementById("ul-el");
+let myLeads = ["www.awesomelead.com","www.epiclead.com", "www.greatlead.com"];
+let listItems = "";
 
-saveBtn.addEventListener("click",function() {
-    console.log("button clicked");
+inputBtn.addEventListener("click",function() {
+    myLeads.push(inputEle.value);
+    // console.log(myLeads);
 })
+
+for(let i = 0; i < myLeads.length; i++) {
+    listItems += "<li>" + myLeads[i] + "</li>";
+    // alternative
+    // const li = document.createElement("li");
+    // li.textContent = myLeads[i];
+    // ulEle.append(li);
+}
+
+ulEle.innerHTML = listItems;
+
+
+// www.awesomelead.com
+// www.epiclead.com
+// www.greatlead.com
