4447328bc6417caa897398c627e4d59b59bfa7ba
Aditi002-holo <aditideshpande2908@gmail.com>
2022-09-18 17:47:36 +0530

    Update comments

--- app.js
+++ app.js
@@ -1,30 +1,37 @@
+// variables declaration
 let myLeads = [];
 
+// target elements
 const inputBtn = document.getElementById("input-btn");
 const inputEle = document.getElementById("input-el");
 const ulEle = document.getElementById("ul-el");
 const deleteBtn = document.getElementById("delete-btn");
 const tabBtn = document.getElementById("tab-btn");
 
-
 const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
 
+// render existing leads
 if(leadsFromLocalStorage) {
     myLeads = leadsFromLocalStorage;
     renderLeads(myLeads);
 }
 
+// save the current tab url
 tabBtn.addEventListener("click", function() {
+    // Using Chrome API
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        // tabs format - [{url: ".."}]
         myLeads.push(tabs[0].url);
         localStorage.setItem("myLeads", JSON.stringify(myLeads));
         renderLeads(myLeads);
     })
 })
 
+// render leads on screen
 function renderLeads(leads) {
     let listItems = "";
     for(let i = 0; i < leads.length; i++) {
+        // add an li element
         listItems += `
         <li>
             <a target='_blank' href='${leads[i]}'> 
@@ -35,6 +42,7 @@ function renderLeads(leads) {
     ulEle.innerHTML = listItems;
 }
 
+// delete existing leads from screen and local storage
 deleteBtn.addEventListener("dblclick", function() {
     localStorage.clear();
     myLeads = [];
@@ -42,6 +50,7 @@ deleteBtn.addEventListener("dblclick", function() {
     renderLeads(myLeads);
 })
 
+// save manual inputs & render them 
 inputBtn.addEventListener("click",function() {
     myLeads.push(inputEle.value);
     inputEle.value = '';
